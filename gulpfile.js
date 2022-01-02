7e66fe07016026b48aa244b09e6831bcae817ba0
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2022-01-02 17:07:38 +0200

    add gallery

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
 import gulp from "gulp"
-import {path} from "./gulp/config/path.js"
-import {copy} from "./gulp/tasks/copy.js"
-import {reset} from "./gulp/tasks/reset.js";
-import {html} from "./gulp/tasks/html.js"
-import {plugins} from "./gulp/config/plugins.js"
-import {server} from "./gulp/tasks/server.js";
-import {scss} from "./gulp/tasks/scss.js"
-import {js} from "./gulp/tasks/js.js"
-import {images} from "./gulp/tasks/images.js";
-import {otfToTtf, ttfToWoff, fontsStyle} from "./gulp/tasks/fonts.js";
-import {svgSprive} from "./gulp/tasks/svgSprite.js";
-import {zip} from "./gulp/tasks/zip.js"
-import {ftp} from "./gulp/tasks/ftp.js"
+import { path } from "./gulp/config/path.js"
+import { copy } from "./gulp/tasks/copy.js"
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js"
+import { plugins } from "./gulp/config/plugins.js"
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js"
+import { js } from "./gulp/tasks/js.js"
+import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSprive } from "./gulp/tasks/svgSprite.js";
+import { zip } from "./gulp/tasks/zip.js"
+import { ftp } from "./gulp/tasks/ftp.js"
 
 
 global.app = {
@@ -30,7 +30,7 @@ function watcher() {
   gulp.watch(path.watch.images, images)
 }
 
-export {svgSprive}
+export { svgSprive }
 
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle)
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images))
@@ -40,9 +40,9 @@ const build = gulp.series(reset, mainTasks)
 const deployZip = gulp.series(reset, mainTasks, zip)
 const deployFTP = gulp.series(reset, mainTasks, ftp)
 
-export {dev}
-export {build}
-export {deployZip}
-export {deployFTP}
+export { dev }
+export { build }
+export { deployZip }
+export { deployFTP }
 
 gulp.task('default', dev)
