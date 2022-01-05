d871b4b73e9f0a23e7ea3e52ea0ba234fc02a62b
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2022-01-05 20:25:03 +0200

    added modal gift

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,51 @@
 import * as flsFunctions from "./modules/functions.js"
 import Swiper, { Navigation, Pagination } from 'swiper';
-
-flsFunctions.isWebp()
-const swiper = new Swiper()
-
+const popupLink = document.querySelector('.certificate__link')
+const popupLinkBurger = document.querySelector('.certificate__link-burger')
+const certificate = document.querySelector('.popup')
+const content = document.querySelector('.popup__content')
+const popupBtn = document.querySelector('.popup__btn')
+const popupImg = document.querySelector('.popup__img')
+const popupImg2 = document.querySelector('.popup__img2')
 const burger = document.querySelector('.burger')
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
+  burger.classList.toggle('active')
 })
+
+popupLink.addEventListener('click', (e) => {
+  console.log('open')
+  certificate.classList.add('open')
+  e.preventDefault()
+})
+
+popupLinkBurger.addEventListener('click', (e) => {
+  console.log('open')
+  certificate.classList.add('open')
+  e.preventDefault()
+})
+
+window.addEventListener('click', (e) => {
+  if (e.target == certificate) {
+    certificate.classList.remove('open')
+    console.log('close');
+  }
+})
+
+
+
+popupBtn.addEventListener('click', (e) => {
+  content.style.transform = 'rotateY(180deg)'
+  popupBtn.style.display = 'none'
+  popupImg.style.display = 'none'
+  popupImg2.style.transform = 'rotateY(180deg)'
+  popupImg2.style.display = 'block'
+  e.preventDefault()
+})
+
+
+
+
+
+flsFunctions.isWebp()
+const swiper = new Swiper()
